b417d1abe3b752a80738d2686232d7171ab3d331
DanielMarj <danielmarjerrison12345@gmail.com>
2021-10-27 00:24:58 -0400

    first few components, starting working on the basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MasterComponent } from './master/master.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
+import { HeaderComponent } from './header/header.component';
+import { CenterViewComponent } from './center-view/center-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterComponent,
+    LeftNavComponent,
+    HeaderComponent,
+    CenterViewComponent
   ],
   imports: [
     BrowserModule,
